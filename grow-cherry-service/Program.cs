f78a645804493bca687d3056dd3dfd44a2c2a51e
yizharGoldstein <izhargo@gmail.com>
2020-04-01 17:01:12 +0300

    first commit

---
+++ grow-cherry-service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace grow_cherry_service
+{
+    internal staticclass Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
